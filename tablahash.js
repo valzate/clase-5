08a77719dc2681f5c846a4f0e4bed12d23e0c498
valzate <valzate223@gmail.com>
2025-08-04 20:50:09 -0500

    primer commit

---
+++ tablahash.js
@@ -0,0 +1,10 @@
+class Hashtable{
+    constructor(size) {
+        this.table = new Array(size); /* */
+        this.size = size;
+        
+    }
+    /*funcion que toma una clave (string) como elemnto y lo 
+    devuelve un indice en la tabla hash*/
+    
+}
